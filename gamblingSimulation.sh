4a2303d8641979518ff43f3886dd94cf79e5b350
sayalipawade <sayalipawade05@gmail.com>
2020-03-23 17:37:32 +0530

    Added:Finding the gambler win or loose

--- gamblingSimulation.sh
+++ gamblingSimulation.sh
@@ -1,7 +1,20 @@
 #!/bin/bash -x
 echo "Welcome" 
 
+#constants
+STAKE=100
+BET=1
+
 #variables
-stake=100
-bet=1
+cash=$STAKE
+temp=0
+
+#finding the gambler loose or win
+temp=$((RANDOM%2))
+if [[ $temp -eq 1 ]]
+then
+	cash=$((cash+1))
+else
+	cash=$((cash-1))
+fi
 
