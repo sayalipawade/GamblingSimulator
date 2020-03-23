3fd1fa82e6e2544a2a0ca7795b2faf19ebe8054d
sayalipawade <sayalipawade05@gmail.com>
2020-03-23 22:29:13 +0530

    Added:Calculating daily cash of gambler

--- gamblingSimulation.sh
+++ gamblingSimulation.sh
@@ -4,19 +4,29 @@ echo "Welcome"
 #constants
 STAKE=100
 BET=1
+DAYS=20
+UPPER_LIMIT=$(($STAKE+(50*STAKE)/100))
+LOWER_LIMIT=$(($STAKE-(50*STAKE)/100))
 
 #variables
 cash=$STAKE
 temp=0
+every_day_cash=0
 
-#finding the gambler loose or win
-temp=$((RANDOM%2))
-if [[ $temp -eq 1 ]]
-then
-	cash=$((cash+(50*cash)/100))
-else
-	cash=$((cash-(50*cash)/100))
-fi
+#finding the gambler's daily cash
+while [ $cash -gt $LOWER_LIMIT ] && [ $cash -lt $UPPER_LIMIT ]
+do
+	temp=$((RANDOM%2))
+	if [ $temp -eq 1 ]
+	then
+		cash=$((cash+BET))
+	else
+		cash=$((cash-BET))
+	fi
+done
+every_day_cash=$((cash-STAKE))
+
+	 
 
 
 
