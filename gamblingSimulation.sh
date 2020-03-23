1b94da80e74f15d20e6a0cd23912f355a1424da6
sayalipawade <sayalipawade05@gmail.com>
2020-03-23 19:44:50 +0530

    Added:Finding the gambler won or lost,if won or lost 50% of the stake would resign fothe day

--- gamblingSimulation.sh
+++ gamblingSimulation.sh
@@ -5,4 +5,19 @@ echo "Welcome"
 STAKE=100
 BET=1
 
+#variables
+cash=$STAKE
+temp=0
+
+#finding the gambler loose or win
+temp=$((RANDOM%2))
+if [[ $temp -eq 1 ]]
+then
+	cash=$((cash+(50*cash)/100))
+else
+	cash=$((cash-(50*cash)/100))
+fi
+
+
+
 
