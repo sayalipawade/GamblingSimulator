be53ae77cb2e1fe09ec0d7747ff3624244e52b41
sayalipawade <sayalipawade05@gmail.com>
2020-03-24 16:14:39 +0530

    Added:Finding out how many days gambler won or lost and by how much amount

--- gamblingSimulation.sh
+++ gamblingSimulation.sh
@@ -3,7 +3,7 @@ echo "Welcome"
 
 #constants
 STAKE=100
-BET=1
+BET=20
 DAYS=20
 UPPER_LIMIT=$(($STAKE+(50*STAKE)/100))
 LOWER_LIMIT=$(($STAKE-(50*STAKE)/100))
@@ -15,31 +15,41 @@ every_day_cash=0
 monthly_cash=0
 totalWinningCash=0
 totalLoosingCash=0
+win=0
+loose=0
+total_won=0
+total_lost=0
 
-#calculating gamblers total amount after 20 days
+#Finding the days won and lost and by how much
 for(( days=1;days<=20;days++))
 do
-	while [ $cash -gt $LOWER_LIMIT ] && [ $cash -lt $UPPER_LIMIT ]
+	cash=$STAKE
+	while [[ $cash -gt $LOWER_LIMIT && $cash -lt $UPPER_LIMIT ]]
 	do
    	temp=$((RANDOM%2))
    	if [ $temp -eq 1 ]
    	then
 			cash=$((cash+BET))
 			totalWinningCash=$((totalWinningCash+BET))
-   	else
+		else
       	cash=$((cash-BET))
 			totalLoosingCash=$((totalLoosingCash+BET))
-   	fi
+		fi
 	done
+	total_won=$((total_won+$totalWinningCash))
+	total_lost=$((total_lost+$totalLoosingCash))
+	if [[ $total_won -gt $total_lost ]]
+	then
+		total_Amount1=$((total_won-total_lost))
+		((win++))
+	else
+		total_Amount1=$((total_lost-total_won))
+		((loose++))
+	fi
 done
-echo $cash
-echo "Total Winning Amount:$totalWinningCash"
-echo "Total Lost Amount:$totalLoosingCash"
-
-
-
-
-
+echo "Winning days:$win"
+echo "Loosing days:$loose"
+echo "Total amount:$total_Amount1"
 	 
 
 
