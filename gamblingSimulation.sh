e62e625e666a23b32c6bb3c7da743b0f7548d07d
sayalipawade <sayalipawade05@gmail.com>
2020-03-24 13:06:03 +0530

    Added:calculating total winning or lost amount of gambler after 20 days

--- gamblingSimulation.sh
+++ gamblingSimulation.sh
@@ -12,19 +12,30 @@ LOWER_LIMIT=$(($STAKE-(50*STAKE)/100))
 cash=$STAKE
 temp=0
 every_day_cash=0
+monthly_cash=0
 
-#finding the gambler's daily cash
-while [ $cash -gt $LOWER_LIMIT ] && [ $cash -lt $UPPER_LIMIT ]
+#calculating gamblers total amount after 20 days
+for(( days=1;days<=20;days++))
 do
-	temp=$((RANDOM%2))
-	if [ $temp -eq 1 ]
-	then
-		cash=$((cash+BET))
-	else
-		cash=$((cash-BET))
-	fi
+	while [ $cash -gt $LOWER_LIMIT ] && [ $cash -lt $UPPER_LIMIT ]
+	do
+   	temp=$((RANDOM%2))
+   	if [ $temp -eq 1 ]
+   	then
+      	cash=$((cash+BET))
+			totalWinningCash=$((totalWinningCash+BET))
+   	else
+      	cash=$((cash-BET))
+			totalLoosingCash=$((totalLoosingCash+BET))
+   	fi
+	done
 done
-every_day_cash=$((cash-STAKE))
+echo $cash
+echo "Total Winning Amount:$totalWinningCash"
+echo "Total Lost Amount:$totalLoosingCash"
+	
+
+
 
 	 
 
